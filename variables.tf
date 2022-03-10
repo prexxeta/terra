227f8e3a93354e5b30d3896f3965db7130236f5c
prexxeta <101182886+prexxeta@users.noreply.github.com>
2022-03-10 12:54:51 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+  type = string
+  description = "Azure subscription ID"
+}
+
+variable "tenant_id" {
+  type = string
+  description = "Azure Tenant ID"
+}
+
+variable "client_id" {
+  type = string
+  description = "Azure client (application) ID"
+}
+
+variable "client_secret" {
+  type = string
+  description = "Azure client (application) secret"
+}
