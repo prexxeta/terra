53b6e8f4b67efab76c82b8932b9a738bedd123fb
prexxeta <101182886+prexxeta@users.noreply.github.com>
2022-03-18 14:37:35 +0100

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  
+  subscription_id   = var.subscription_id
+  tenant_id         = var.tenant_id
+  client_id         = var.client_id
+  client_secret     = var.client_secret  
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "azure-functions-example-rg"
   location = "West Europe"
